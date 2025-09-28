eb158c35b49dbe64e90049afa55eb580ccbfd013
Eliya <eliyanoah70@gmail.com>
2025-09-28 20:33:38 +0300

    Added basic CRUD and styles

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
+
+/* Connect to the database */
+mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+}); 
+
+/* Set up Express app */
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+app.engine('ejs', ejsMate);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+/* Define routes */
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const newCampground = new Campground(req.body.campground);
+  await newCampground.save();
+  res.redirect(`/campgrounds/${newCampground._id}`);
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {  
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  await Campground.findByIdAndDelete(req.params.id);
+  res.redirect('/campgrounds');
+});
+
---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelper');
+/* Connect to the database */
+mongoose.connect('mongodb://localhost:27017/yelp-camp').then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+}); 
+
+
+async function seedDB() {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {  
+    const randomCity = Math.floor(Math.random() * cities.length);
+    const randomDescriptor = Math.floor(Math.random() * descriptors.length);
+    const randomPlace = Math.floor(Math.random() * places.length);
+    const camp = new Campground({
+      location: `${cities[randomCity].city}, ${cities[randomCity].state}`,
+      title: `${descriptors[randomDescriptor]} ${places[randomPlace]}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: Math.floor(Math.random() * 20) + 10
+    });
+    await camp.save();
+  }
+}
+
+seedDB().then(() => {
+  console.log('Database seeded successfully!');
+  mongoose.connection.close();
+});
