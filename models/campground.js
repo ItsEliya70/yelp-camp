4b80f6288b693cd7a1bdd91babe59137c551721a
Eliya <eliyanoah70@gmail.com>
2025-09-30 14:28:40 +0300

    Created a review model

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
-const CampgroundSchema = new mongoose.Schema({
+const campgroundSchema = new mongoose.Schema({
   title: String,
   price: Number,
   image: String,
   description: String,
-  location: String
+  location: String,
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review'
+    }
+  ]
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model('Campground', campgroundSchema);
