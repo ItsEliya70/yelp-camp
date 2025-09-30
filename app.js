90322a607287588479c0b0392eea5ec3c6c07d9c
Eliya <eliyanoah70@gmail.com>
2025-09-30 15:02:56 +0300

    Added the creation of reviews logic (path /campgrounds/:id/reviews)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { campgroundSchema } = require('./schemas');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const catchAsync = require('./utils/catchAsync');
-
+const Review = require('./models/review');
 
 
 /* Connect to the database */
@@ -84,6 +84,15 @@ app.delete('/campgrounds/:id', async (req, res) => {
   await Campground.findByIdAndDelete(req.params.id);
   res.redirect('/campgrounds');
 });
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}));
 
 
 app.all(/(.*)/, (req, res, next) => {
